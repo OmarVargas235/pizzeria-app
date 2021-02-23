3446ea463518253d56e27a5432ab83b84eedd0b3
OmarVargas235 <ov1356272@gmail.com>
2021-02-23 08:52:47 -0400

    Agregando los estilos del componente Login

---
+++ src/components/login/style.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+
+export const LoginContainer = styled.section `
+
+	form {
+		width: 300px;
+	}
+
+	input {
+		height: 53px;
+		border: none;
+
+		&:focus {
+			box-shadow: none;
+		}
+	}
+	
+	.input-group {
+		border: 1px solid #909090;
+		border-radius: .25rem;
+	}
+
+	.welcome {
+		font-size: 2rem;
+	}
+
+	.welcome+p {
+		color: #909090;
+		font-size: 1.1rem;
+	}
+
+	.icon {
+		width: 25px;
+		height: 25px;
+	}
+
+	.btn-login {
+		width: 300px;
+		padding: 10px 0;
+		box-sizing: border-box;
+		box-shadow: 0 4px 4px rgba(0,0,0,.15);
+	}
+
+	.btn-warning {
+		background: #ffd970;
+		border-color: #ffd970;
+
+		&:active {
+			background: #ffd970 !important;
+			border-color: #ffd970 !important;
+			box-shadow: 0 4px 4px rgba(0,0,0,.15) !important;
+		}
+	}
+
+	img {
+		width: 40%;
+	}
+
+	@media (min-width: 450px) {		
+		img {
+			width: 30%;
+		}
+	}
+
+	@media (min-width: 576px) {		
+		img {
+			width: 20%;
+		}
+	}
+
+	@media (min-width: 768px) {		
+		img {
+			width: 40%;
+		}
+	}
+`;
