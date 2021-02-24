2e3c7cdd4d1aeae9328ac23dd5b2fcff13b6ff13
OmarVargas235 <ov1356272@gmail.com>
2021-02-24 12:57:54 -0400

    Creando el customHook useFetch para el consumo de la data

---
+++ src/customHooks/useFetch.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+export const useFetch = (url="") => {
+	
+	const [data, setData] = useState({ data: null, loading: true, error: null });
+
+	useEffect(() => {
+		
+		async function consumeAPI() {
+			
+			try {
+
+				const resp = await fetch(`http://localhost:5000/${url}`);
+				const result = await resp.json();
+
+				if (resp.status !== 200) {
+
+					throw new Error(result.error);
+					return;
+				}
+
+				const { data, ok } = result;
+				setData({ data, loading: false, error: null });
+				
+			} catch(err) {
+
+				err = (/Failed to fetch/gi).test(err) ? 'server failure' : err;
+				setData({ data: null, loading: true, error: err });
+			}
+		}
+
+		consumeAPI();
+
+	}, []);
+
+	return data;
+}
+
+export default useFetch;
