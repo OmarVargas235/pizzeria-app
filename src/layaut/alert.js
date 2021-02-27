ec73439d7b302a22e8d1b87f031bc56fb53cfa40
OmarVargas235 <ov1356272@gmail.com>
2021-02-27 17:34:12 -0400

    Agregando funcionalidad del alert

---
+++ src/layaut/alert.js
@@ -0,0 +1,16 @@
+import Swal from 'sweetalert2';
+
+export const alert = (iconError, message) => {
+
+	const Toast = Swal.mixin({
+		toast: true,
+		position: 'top',
+		showConfirmButton: false,
+		timer: 3000,
+	})
+
+	Toast.fire({
+		icon: iconError,
+		title: message,
+	});
+}
